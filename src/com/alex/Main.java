052359b64d1c16e66b4be72ed33a47715b43f151
Leo <stbirb@yandex.ru>
2019-05-09 15:52:02 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,12 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char reserved = '\u00AE';
+        System.out.println("reserved = "+reserved);
+
+	// write your code here
+    }
+}
